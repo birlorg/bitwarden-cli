1ec22a6efdd3bd52211ae4186b26f50f35e372a6
zie <zie>
2018-04-10 20:59:30 +0000

    remove fsl and git marks and move them out of the repo, they are ANNOYING to look at.

--- tools/fsl2git.sh
+++ tools/fsl2git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # docs see: http://fossil-scm.org/index.html/doc/trunk/www/inout.wiki
 cd ~/src/bitwarden/bitwarden-cli.git
-fossil export --git --import-marks ../bitwarden-cli/fossil.marks  \
-       --export-marks ../bitwarden-cli/fossil.marks               \
+fossil export --git --import-marks ~/fossil/bitwarden-cli-fossil.marks  \
+       --export-marks ~/fossil/bitwarden-cli-fossil.marks               \
        ~/fossil/bitwarden-cli.fossil | git fast-import            \
-       --import-marks=../bitwarden-cli/git.marks                  \
-       --export-marks=../bitwarden-cli/git.marks
+       --import-marks=~/fossil/bitwarden-cli-git.marks                  \
+       --export-marks=~/fossil/bitwarden-cli-git.marks
--- tools/git2fsl.sh
+++ tools/git2fsl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/src/bitwarden/bitwarden-cli.git
-git fast-export --import-marks=../bitwarden-cli/git.marks                  \
-    --export-marks=../bitwarden-cli/git.marks --all | fossil import --git  \
-    --incremental --import-marks ../bitwarden-cli/fossil.marks             \
-    --export-marks ../bitwarden-cli/fossil.marks ~/fossil/bitwarden-cli.fossil
+git fast-export --import-marks=~/fossil/bitwarden-cli-git.marks                  \
+    --export-marks=~/fossil/bitwarden-cli-git.marks --all | fossil import --git  \
+    --incremental --import-marks ~/fossil/bitwarden-cli-fossil.marks             \
+    --export-marks ~/fossil/bitwarden-cli-fossil.marks ~/fossil/bitwarden-cli.fossil
