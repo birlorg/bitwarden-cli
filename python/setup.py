a9b30c511618aa140957636268e351c734993f9f
zie <zie>
2018-04-12 00:37:26 +0000

    fix install_requires bump to v0.3.1

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,7 @@ import os
 
 from setuptools import setup
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 
 
 def readme():
@@ -37,12 +37,12 @@ setup(
     install_requires=[
         "click==6.7",
         "cryptography==2.2.2",
-        "objectpath==0.5",
         "psutil==5.4.3",
         "pystandardpaths==0.3.2",
         "records==0.5.2",
         "requests==2.18.4",
         "tablib==0.12.1",
+        "python-daemon"
     ],
     # install_requires=read_requirements('requirements.txt'),
     tests_require=read_requirements('requirements-tests-py3.txt'),
