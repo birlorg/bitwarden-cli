4bc25b69700c39797abf81e49827cf04a8a6e2ca
zie <bitwarden@birl.ca>
2018-04-19 23:20:55 +0000

    add circleCI stuff that might work

--- rust/src/crypto.rs
+++ rust/src/crypto.rs
@@ -18,7 +18,6 @@ pub fn hmac_openssl(key: &[u8], messages: &str) -> String {
     return result;
 }
 
-
 //make a master key.
 pub fn make_key(password: &str, salt: &str) -> [u8; 32] {
     // 256-bit derived key
@@ -36,11 +35,87 @@ pub fn make_key(password: &str, salt: &str) -> [u8; 32] {
 pub fn hashed_password(password: &str, salt: &str) -> String {
     let key = make_key(password, salt);
     //let mut derived_key = [0u8; 32];
-    // let mut derived_key = [0; 32];
-    let mut  derived_key = openssl::pkcs5::pbkdf2_hmac(&password.as_bytes(),salt.as_bytes(), 1, openssl::hash::MessageDigest::sha256(), &mut key).unwrap();
+    let mut derived_key = [0; 32];
+    openssl::pkcs5::pbkdf2_hmac(&key,&password.as_bytes(), 1, openssl::hash::MessageDigest::sha256(), &mut derived_key).unwrap();
     let mut result = String::from("");
     result.push_str(&derived_key.to_base64(base64::STANDARD)[..]);
     return result;
+}
+
+// encode into a bitwarden compatible cipher string.
+pub fn encode_cipher_string(enctype: &u8, iv: &[u8], ct: &[u8], mac: &[u8]) -> String {
+    let mut result = String::from("");
+    result.push_str(&enctype.to_string());
+    result.push_str(".");
+    if iv.len() > 0 {
+        result.push_str(&iv.to_base64(base64::STANDARD)[..]);
+    }
+    if ct.len() > 0 {
+        result.push_str("|");
+        result.push_str(&ct.to_base64(base64::STANDARD)[..]);
+    }
+    if mac.len() > 0 {
+        result.push_str("|");
+        result.push_str(&mac.to_base64(base64::STANDARD)[..]);
+    }
+    return result;
+}
+pub struct Cipherstring {
+    encryption_type: u8,
+    iv: Vec<u8>,
+    ct: Vec<u8>,
+    mac: Vec<u8>,
+}
+
+//decode a bitwarden cipher string
+pub fn decode_cipher_string(cipher_string: &str) -> Cipherstring {
+    let pieces: Vec<&str> = cipher_string.split("|").collect();
+    let beg = pieces[0];
+    let beg_pieces: Vec<&str> = beg.split(".").collect();
+    let enc_type = beg_pieces[0];
+    let iv = beg_pieces[1];
+    let ct = pieces[1];
+    let mut mac = vec![0; 0];
+    if pieces.len() == 3 {
+        mac = pieces[2].from_base64().unwrap();
+    } else {
+        mac = [0; 0].to_vec();
+    };
+    let result = Cipherstring {
+        encryption_type: enc_type.parse::<u8>().unwrap(),
+        iv: iv.from_base64().unwrap(),
+        ct: ct.from_base64().unwrap(),
+        mac,
+    };
+    return result;
+}
+
+//create symmetric key (encryption_key and mac_key from secure random bytes
+// we return it as one large 64 byte variable, the first 32 are the encryption key and the last 32 are the mac_key.
+pub fn symmetric_key() -> (Vec<u8>) {
+    let mut rng = rand::thread_rng();
+    let encryption_key: Vec<u8> = rand::seq::sample_iter(&mut rng, 0..u8::max_value(), 32).unwrap();
+    let mac_key: Vec<u8> = rand::seq::sample_iter(&mut rng, 0..u8::max_value(), 32).unwrap();
+    return encryption_key + mac_key;
+}
+
+// make encryption key
+pub fn make_encrypted_key(symmetric_key: Vec<u8>, master_key: [u8; 32]) -> String {
+    let mut rng = rand::thread_rng();
+    let iv: Vec<u8> = rand::seq::sample_iter(&mut rng, 0..u8::max_value(), 16).unwrap();
+    let cipher = encrypt_aes_256_cbc(&symmetric_key, &master_key, &iv).unwrap();
+    let mac: [u8; 0] = [];
+    let ret = encode_cipher_string(&0, &iv, &cipher, &mac);
+    return ret;
+}
+
+//double hmac compare.
+pub fn macs_equal(mac_key: &[u8], mac1: &[u8], mac2: &[u8]) -> bool {
+    let mut hmac1 = hmac::Hmac::new(crypto::sha2::Sha256::new(), &mac_key);
+    hmac1.input(&mac1);
+    let mut hmac2 = hmac::Hmac::new(crypto::sha2::Sha256::new(), &mac_key);
+    hmac2.input(&mac2);
+    return hmac1.result() == hmac2.result();
 }
 
 #[cfg(test)]
